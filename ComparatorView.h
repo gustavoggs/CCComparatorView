7b1bb0494bda62a840587d8b258e0929b661bda3
Gustavo Grana <gustavoggs@gmail.com>
2012-07-04 10:03:46 -0300

    The ComparatorView.h and ComparatorView.m class first commit

---
+++ ComparatorView.h
@@ -0,0 +1,12 @@
+//
+//  ComparatorView.h
+//  Syngo
+//
+//  Created by Gustavo Grana on 7/3/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComparatorView : UIImageView
+
+@end
---
+++ ComparatorView.m
@@ -0,0 +1,67 @@
+//
+//  PixelPerfectView.m
+//  Syngo
+//
+//  Created by Gustavo Grana on 7/3/12.
+//
+
+#import "ComparatorView.h"
+
+@implementation ComparatorView
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        UIView* touchView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+        UIPanGestureRecognizer* pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(gestureAlphaChange:)];
+        [pan setMaximumNumberOfTouches:1];
+        UIPanGestureRecognizer* panRemove = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(gestureRemove:)];
+
+        [panRemove setMinimumNumberOfTouches:2];
+        
+        [self setBackgroundColor:[UIColor clearColor]];
+        [touchView addGestureRecognizer:panRemove];
+        [touchView addGestureRecognizer:pan];
+        [self addSubview:touchView];
+        
+    }
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+
+    self = [super initWithFrame:frame];
+    if (self) {
+        UIView* touchView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+        UIPanGestureRecognizer* pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(gestureAlphaChange:)];
+        [pan setMaximumNumberOfTouches:1];
+        UIPanGestureRecognizer* panRemove = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(gestureRemove:)];
+        
+        [panRemove setMinimumNumberOfTouches:2];
+        
+        [self setBackgroundColor:[UIColor clearColor]];
+        [touchView addGestureRecognizer:panRemove];
+        [touchView addGestureRecognizer:pan];
+        [self addSubview:touchView];
+        
+    }
+    return self;
+}
+
+- (void) gestureAlphaChange:(UIPanGestureRecognizer*)pan {
+
+    CGPoint velocity = [pan velocityInView:self];
+    if (velocity.y < 0) {
+        self.alpha = self.alpha == 1 ? 1 : self.alpha + 0.01;
+    } else {        
+        self.alpha = self.alpha < 0.05 ? 0.05 : self.alpha - 0.01;
+    }
+}
+
+- (void) gestureRemove:(UIPanGestureRecognizer*)pan {
+    self.alpha = 0;
+}
+
+@end
